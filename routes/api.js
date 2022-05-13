e22196edf7a9687d65249c76b95d2ef076015124
Izdrathi <agn.gawron@gmail.com>
2022-05-13 11:29:51 +0100

    basic GET /api/recipies functionality

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,15 @@
-const apiRouter = require('express').Router();
+const apiRouter = require("express").Router();
+const { readFile } = require("fs/promises");
 
-apiRouter.get('/', (_, res) => {
-  res.json({ message: 'ok' });
+apiRouter.get("/", (_, res) => {
+    res.json({ message: "ok" });
+});
+
+apiRouter.get("/recipies", (req, res) => {
+    readFile("./data/data.json", "utf-8").then((fileContents) => {
+        const recipies = JSON.parse(fileContents);
+        res.status(200).send({ recipies });
+    });
 });
 
 module.exports = apiRouter;
--- test/api.test.js
+++ test/api.test.js
@@ -1,9 +1,23 @@
-const supertest = require('supertest');
-const server = require('../server');
+const supertest = require("supertest");
+const server = require("../server");
 
 const request = supertest(server);
 
-test('/api', async () => {
-  const { body } = await request.get('/api').expect(200);
-  expect(body.message).toBe('ok');
+test("/api", async () => {
+    const { body } = await request.get("/api").expect(200);
+    expect(body.message).toBe("ok");
+});
+
+test("/api/recipies", async () => {
+    const { body } = await request.get("/api/recipies").expect(200);
+    body.recipies.forEach((recipie) => {
+        expect(recipie).toEqual(
+            expect.objectContaining({
+                id: expect.any(String),
+                imageUrl: expect.any(String),
+                instructions: expect.any(String),
+                ingredients: expect.any(Array),
+            })
+        );
+    });
 });
